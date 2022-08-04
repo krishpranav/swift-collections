3449e3bed12b9d040914f32cb578bf4a53abcc6e
krishpranav <krisna.pranav@gmail.com>
2022-08-04 18:50:59 +0530

    FetchAPI: FetchJson response

---
+++ FetchAPI/FetchJson/FetchJson/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FetchJson
+//
+//  Created by Pranav on 04/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
